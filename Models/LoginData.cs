025d78f79fac5c13e8724b4e69795a118d7a1f56
Hafijul Islam <hafijul233@gmail.com>
2018-09-21 19:09:57 +0600

    Models Added

---
+++ Models/LoginData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class LoginData
+    {
+        public string EmailAddress { get; set; }
+        public string PassWord { get; set; }
+
+    }
+}
--- Models/Participant.cs
+++ Models/Participant.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+    class Participant
     {
     }
 }
