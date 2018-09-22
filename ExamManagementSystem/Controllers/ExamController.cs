1f9f71e0938c16e3b6efe2c1a3501d9acc3536e4
Hafijul Islam <hafijul233@gmail.com>
2018-09-23 01:57:29 +0600

    Entry page Add and Reference HTML Code Added

---
+++ ExamManagementSystem/Controllers/ExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamManagementSystem.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
