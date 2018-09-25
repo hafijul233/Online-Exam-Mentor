8dfd2c49874aaa232b91ef5a89ac1023d5ab8166
Hafijul Islam <hafijul233@gmail.com>
2018-09-25 19:35:00 +0600

    batch Controller

---
+++ BLL/BatchManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Models;
+using Repository;
+
+namespace ExamManagementSystem.Controllers
+{
+    public class BatchManager
+    {
+        BatchRepository newBatchRepository = new BatchRepository();
+       public bool ManagerEntry(Batch newBatch)
+       {
+            int confirm = newBatchRepository.RepositoyEntry(newBatch);
+
+           if (confirm > 0)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+
+    }
+}
--- ExamManagementSystem/Controllers/BatchController.cs
+++ ExamManagementSystem/Controllers/BatchController.cs
@@ -3,22 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using BLL;
 
 namespace ExamManagementSystem.Controllers
 {
     public class BatchController : Controller
     {
+        BatchManager newBatchManager = new BatchManager();
         // GET: Batch
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Entry()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Entry(Batch newBatch)
+        {
+            bool isAdded;
+
+            if (ModelState.IsValid)
+            {
+                isAdded = newBatchManager.ManagerEntry(newBatch);
+
+                if (isAdded == true)
+                {
+                    ViewBag.Organization = newBatch;
+
+                    return View("Information");
+                }
+
+                ViewBag.Organization = newBatch;
+
+                return View("Information");
+
+            }
+
+            ViewBag.Organization = newBatch;
+
+            return View();
+        }
         public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Information()
         {
             return View();
         }
