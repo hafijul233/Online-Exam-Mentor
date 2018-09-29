f30696d4227d04c353cb466e629dce2beaac0dc8
Hafijul Islam <hafijul233@gmail.com>
2018-09-30 00:10:05 +0600

    db update

---
+++ DBContext/Migrations/201809291805137_reset.cs
@@ -0,0 +1,18 @@
+namespace DBContext.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class reset : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Participants", "RegNo", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Participants", "RegNo");
+        }
+    }
+}
